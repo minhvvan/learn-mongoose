9f40fc790d56a2ba208ede07f1558f5c604423ca
GR8 <eminhwan12@gmail.com>
2021-02-19 13:57:48 +0900

    finish

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 
 const connect = require('./schemas');
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
 
 const app = express();
+
 app.set('port', process.env.PORT || 3002);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
@@ -17,7 +21,12 @@ connect();
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+app.use(express.urlencoded({extended: false}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 app.use((req, res, next) => {
     const err = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
